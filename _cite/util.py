784a82a9ed6ffca9add4a868321e6745810b0306
kimds <gratia__@naver.com>
2026-03-18 14:51:22 +0900

    correct prefix

--- _cite/util.py
+++ _cite/util.py
@@ -151,6 +151,10 @@ def cite_with_manubot(source):
 
     # source id
     id = source.get("id")
+    
+    # auto-correct incorrect doi.org prefix to standard doi: prefix
+    if id and id.startswith("doi.org/"):
+        id = id.replace("doi.org/", "doi:")
 
     # run Manubot
     try:
@@ -171,10 +175,6 @@ def cite_with_manubot(source):
 
     # original id
     citation["id"] = id
-    # auto-correct incorrect doi.org prefix to standard doi: prefix
-    if id and id.startswith("doi.org/"):
-        id = id.replace("doi.org/", "doi:")
-        source["id"] = id # update the original source dict
 
     # title
     citation["title"] = manubot.get("title", "")
